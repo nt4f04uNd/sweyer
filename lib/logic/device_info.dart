d7f61583d458c2ab2ed6c900c3b9f5bed231e87e
Abestanis <abestanis.gc@gmail.com>
2022-06-14 20:39:14 +0200

    Replace device_info by device_info_plus

--- lib/logic/device_info.dart
+++ lib/logic/device_info.dart
@@ -1,4 +1,4 @@
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:sweyer/sweyer.dart';
 
 /// Provides an information about the device.
@@ -19,6 +19,6 @@ class DeviceInfoControl extends Control {
   Future<void> init() async {
     super.init();
     final androidInfo = await DeviceInfoPlugin().androidInfo;
-    _sdkInt = androidInfo.version.sdkInt;
+    _sdkInt = androidInfo.version.sdkInt!;
   }
 }
