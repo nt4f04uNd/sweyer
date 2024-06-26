b96a5ee3eead62e546059a13b4071c59f7a11454
Abestanis <abestanis.gc@gmail.com>
2024-06-27 01:04:23 +0200

    Update theme_tailor (#119)
    * Update theme_tailor
    
    * Mark some files as generated
    
    * Fix AppTheme.glowSplashColorOnContrast
    
    * Actually use the modalOverGreySystemNavigationBarColor

--- lib/constants/theme/system_ui_theme.dart
+++ lib/constants/theme/system_ui_theme.dart
@@ -6,85 +6,104 @@ import '../colors.dart';
 
 part 'system_ui_theme.tailor.dart';
 
-@tailor
-class _$SystemUiTheme {
+@TailorMixin()
+class SystemUiTheme extends ThemeExtension<SystemUiTheme> with _$SystemUiThemeTailorMixin {
+  const SystemUiTheme({
+    required this.black,
+    required this.grey,
+    required this.drawerScreen,
+    required this.bottomSheet,
+    required this.modal,
+    required this.modalOverGreySystemNavigationBarColor,
+  });
+
+  /// Theme where nav bar is [grey] (with default dark theme).
+  /// For light this means [eee].
+  ///
+  /// The opposite is [black].
+  @override
+  final SystemUiOverlayStyle grey;
+
   /// Default theme for all screens.
   ///
   /// Theme where nav bar is [black] (with default dark theme).
   /// For light this means [white].
   ///
   /// The opposite is [grey].
-  static final black = [
-    /// [withOpacity] needed for smooth transition to [drawerScreen].
-    SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.white,
-      systemNavigationBarDividerColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.dark,
-      statusBarColor: Colors.white.withOpacity(0.0),
-      statusBarBrightness: Brightness.dark,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-    SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.black,
-      systemNavigationBarDividerColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.light,
-      statusBarColor: AppColors.grey.withOpacity(0.0),
-      statusBarBrightness: Brightness.light,
-      statusBarIconBrightness: Brightness.light,
-    ),
-  ];
-
-  /// Theme where nav bar is [grey] (with default dark theme).
-  /// For light this means [eee].
-  ///
-  /// The opposite is [black].
-  static final grey = [
-    black[0].copyWith(systemNavigationBarColor: AppColors.eee),
-    black[1].copyWith(systemNavigationBarColor: AppColors.grey),
-  ];
+  @override
+  final SystemUiOverlayStyle black;
 
   /// Theme for the drawer screen.
-  static final drawerScreen = [
-    black[0].copyWith(
+  @override
+  final SystemUiOverlayStyle drawerScreen;
+
+  /// Theme for the bottom sheet dialog.
+  @override
+  final SystemUiOverlayStyle bottomSheet;
+
+  /// Theme for the modal dialog.
+  @override
+  final SystemUiOverlayStyle modal;
+
+  @override
+  final Color modalOverGreySystemNavigationBarColor;
+
+  /// Theme for the modal dialog that is displayed over [grey].
+  @override
+  SystemUiOverlayStyle get modalOverGrey => modal.copyWith(
+    systemNavigationBarColor: modalOverGreySystemNavigationBarColor,
+  );
+
+  static final SystemUiOverlayStyle _lightBaseStyle = SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    statusBarColor: Colors.white.withOpacity(0.0),
+    statusBarBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+  );
+
+  static final SystemUiOverlayStyle _darkBaseStyle = SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.black,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarColor: AppColors.grey.withOpacity(0.0),
+    statusBarBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.light,
+  );
+
+  static final SystemUiTheme light = SystemUiTheme(
+    black: _lightBaseStyle,
+    grey: _lightBaseStyle.copyWith(systemNavigationBarColor: AppColors.eee),
+    drawerScreen: _lightBaseStyle.copyWith(
       statusBarColor: Colors.white,
       systemNavigationBarColor: Colors.white,
     ),
-    black[1].copyWith(
-      statusBarColor: AppColors.grey,
-      systemNavigationBarColor: AppColors.grey,
-    ),
-  ];
-
-  /// Theme for the bottom sheet dialog.
-  static final bottomSheet = [
-    black[0].copyWith(
+    bottomSheet: _lightBaseStyle.copyWith(
       systemNavigationBarColor: Colors.white,
       statusBarBrightness: Brightness.light,
       statusBarIconBrightness: Brightness.light,
     ),
-    black[1].copyWith(
-      systemNavigationBarColor: Colors.black,
-    ),
-  ];
-
-  /// Theme for the modal dialog.
-  static final modal = [
-    black[0].copyWith(
+    modal: _lightBaseStyle.copyWith(
       systemNavigationBarColor: const Color(0xff757575),
       systemNavigationBarIconBrightness: Brightness.light,
       statusBarBrightness: Brightness.light,
       statusBarIconBrightness: Brightness.light,
     ),
-    black[1],
-  ];
+    modalOverGreySystemNavigationBarColor: const Color(0xff6d6d6d),
+  );
 
-  /// Theme for the modal dialog that is displayed over [grey].
-  static final modalOverGrey = [
-    modal[0].copyWith(
-      systemNavigationBarColor: const Color(0xff6d6d6d),
+  static final SystemUiTheme dark = SystemUiTheme(
+    black: _darkBaseStyle,
+    grey: _darkBaseStyle.copyWith(systemNavigationBarColor: AppColors.grey),
+    drawerScreen: _darkBaseStyle.copyWith(
+      statusBarColor: AppColors.grey,
+      systemNavigationBarColor: AppColors.grey,
     ),
-    modal[1].copyWith(
-      systemNavigationBarColor: const Color(0xff0d0d0d),
+    bottomSheet: _darkBaseStyle.copyWith(
+      systemNavigationBarColor: Colors.black,
     ),
-  ];
+    modal: _darkBaseStyle,
+    modalOverGreySystemNavigationBarColor: const Color(0xff0d0d0d),
+  );
 }
