087abbbb3d2d252e7b865ceeeabbecc28c12b3a4
Abestanis <abestanis.gc@gmail.com>
2025-04-13 23:13:36 +0200

    Load the Roboto font from the Flutter sdk

--- test/golden/flutter_test_config.dart
+++ test/golden/flutter_test_config.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io' show File;
 
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart' show ByteData, FontLoader, rootBundle;
+import 'package:flutter_test/flutter_test.dart' show TestWidgetsFlutterBinding, setUpAll;
 
 // Load the custom fonts from the assets.
 // From https://pub.dev/documentation/golden_toolkit/latest/golden_toolkit/loadAppFonts.html
 Future<void> loadAppFonts() async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  final roboto = File('test/golden/Roboto-Regular.ttf').readAsBytes().then(ByteData.sublistView);
+  final FontLoader fontLoader = FontLoader('Roboto')..addFont(roboto);
+  await fontLoader.load();
+
   final fontManifest = await rootBundle.loadStructuredData<Iterable<dynamic>>(
     'FontManifest.json',
     (string) async => json.decode(string),
@@ -22,6 +27,7 @@ Future<void> loadAppFonts() async {
 }
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  await loadAppFonts();
   setUpAll(loadAppFonts);
   await testMain();
 }
