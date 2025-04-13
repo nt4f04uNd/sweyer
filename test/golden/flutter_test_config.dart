c7b725f24add11a549881bf963c72375168c093b
Abestanis <abestanis.gc@gmail.com>
2025-04-13 22:17:53 +0200

    Replace `golden_toolkit` with the builtin functionality

--- test/golden/flutter_test_config.dart
+++ test/golden/flutter_test_config.dart
@@ -1,17 +1,27 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// Load fonts for goldens, taken from https://pub.dev/packages/golden_toolkit#loading-fonts
-// Also enable real shadows
-Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  return GoldenToolkit.runWithConfiguration(
-    () async {
-      await loadAppFonts();
-      return testMain();
-    },
-    config: GoldenToolkitConfiguration(
-      enableRealShadows: true,
-    ),
+// Load the custom fonts from the assets.
+// From https://pub.dev/documentation/golden_toolkit/latest/golden_toolkit/loadAppFonts.html
+Future<void> loadAppFonts() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final fontManifest = await rootBundle.loadStructuredData<Iterable<dynamic>>(
+    'FontManifest.json',
+    (string) async => json.decode(string),
   );
+  for (final Map<String, dynamic> font in fontManifest) {
+    final fontLoader = FontLoader(font['family']);
+    for (final Map<String, dynamic> fontType in font['fonts']) {
+      fontLoader.addFont(rootBundle.load(fontType['asset']));
+    }
+    await fontLoader.load();
+  }
+}
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  setUpAll(loadAppFonts);
+  await testMain();
 }
