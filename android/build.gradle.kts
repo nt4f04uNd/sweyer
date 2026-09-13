6fc12d8eb10a40a7364fccbaadc60fc38f1cca7b
nt4f04uNd <nt4f04und@gmail.com>
2026-09-13 03:27:40 +0200

    Pin compatible home widget Android dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,16 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // TODO: Remove these pins once https://github.com/ABausG/home_widget/issues/417
+    //       is closed and the fix is available in a stable home_widget release.
+    configurations.configureEach {
+        resolutionStrategy.force(
+            "androidx.glance:glance-appwidget:1.1.1",
+            "androidx.work:work-runtime:2.9.1",
+            "androidx.work:work-runtime-ktx:2.9.1",
+        )
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
